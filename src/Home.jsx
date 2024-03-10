209f8ae8a785de13cf6bda0e992f6a752a9c98ee
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-03-11 01:48:04 +0500

    completed

--- src/Home.jsx
+++ src/Home.jsx
@@ -4,7 +4,7 @@ import { Header } from "./Component/Header";
 const Home = () => {
   return (
     <div className="grid place-content-center w-full h-[80vh]">
-      <div className="text-[5rem]">Welcome To the BLACKLANE</div>
+      <div className="text-[5rem] font-bold">Welcome To the BLACKLANE</div>
       <div className="w-[70%] m-auto bg-slate-400  h-2 rounded-lg"></div>
       {/* <Header /> */}
     </div>
