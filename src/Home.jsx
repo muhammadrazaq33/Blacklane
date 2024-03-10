8fd4c241da132a44c0d022e6b92c76bcfdea9ba2
muhammadrazaq33 <muhammadrazaq5913483@gmail.com>
2024-03-11 01:57:53 +0500

    responsiveness completed

--- src/Home.jsx
+++ src/Home.jsx
@@ -3,10 +3,14 @@ import { Header } from "./Component/Header";
 
 const Home = () => {
   return (
-    <div className="grid place-content-center w-full h-[80vh]">
-      <div className="text-[5rem] font-bold">Welcome To the BLACKLANE</div>
-      <div className="w-[70%] m-auto bg-slate-400  h-2 rounded-lg"></div>
-      {/* <Header /> */}
+    <div className="max-[1200px] max-w-[91vw] m-auto">
+      <div className="grid place-content-center w-full md:h-[80vh] h-[50vh]">
+        <div className="lg:text-[4rem] md:text-[3rem] sm:text-[2.5rem] text-[1.5rem] font-bold">
+          Welcome To the BLACKLANE
+        </div>
+        <div className="w-[70%] m-auto bg-slate-400  h-2 rounded-lg"></div>
+        {/* <Header /> */}
+      </div>
     </div>
   );
 };
